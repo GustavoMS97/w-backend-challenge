b6a95b453d7f5ded2dc50c98608279c963237027
Gustavo Martins do Santos <gustavo@tfcsolutions.tech>
2024-05-02 13:49:49 -0300

    feat: add the list task controller and action

--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -4,12 +4,14 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { Task, TaskSchema } from '@app/modules/task/task.entity'
 import TaskMongooseRepository from '@app/modules/task/task-mongoose.repository'
 import { TaskCreateController } from '@app/modules/task/controller/task-create.controller'
+import { TaskListController } from '@app/modules/task/controller/task-list.controller'
 import { TaskCreateAction } from '@app/modules/task/action/task-create.action'
+import { TaskListAction } from '@app/modules/task/action/task-list.action'
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }])],
-  providers: [TaskMongooseRepository, TaskCreateAction],
-  exports: [TaskCreateAction],
-  controllers: [TaskCreateController]
+  providers: [TaskMongooseRepository, TaskCreateAction, TaskListAction],
+  exports: [TaskCreateAction, TaskListAction],
+  controllers: [TaskCreateController, TaskListController]
 })
 export class TaskModule {}
