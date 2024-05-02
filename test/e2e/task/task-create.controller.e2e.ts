9091a7eb18b06fca061f144fc62ecb87741a5468
Gustavo Martins do Santos <gustavo@tfcsolutions.tech>
2024-05-02 15:12:56 -0300

    fix: adjust e2e test for task creation so that mongo is no longer mocked, and real data is created into memory server

--- test/e2e/task/task-create.controller.e2e.ts
+++ test/e2e/task/task-create.controller.e2e.ts
@@ -1,41 +1,21 @@
 import * as request from 'supertest'
-import mongoose from 'mongoose'
-import { INestApplication } from '@nestjs/common'
+import { INestApplication, Logger } from '@nestjs/common'
 import { Test, TestingModule } from '@nestjs/testing'
 import { App } from 'supertest/types'
 
 import { MainModule } from '@app/main.module'
-import TaskMongooseRepository from '@app/modules/task/task-mongoose.repository'
-import { Task } from '@app/modules/task/task.entity'
-import { TASK_STATUS } from '@app/modules/task/enum/task-status.enum'
+import { MongooseRepositoryAbstract } from '@app/shared/generic/mongoose-repository.abstract'
 
 describe('TaskCreateController (e2e)', () => {
   let app: INestApplication
-  let taskMongooseRepository: Partial<jest.Mocked<TaskMongooseRepository>>
-
-  const createResultMock = Object.assign(new Task(), {
-    _id: new mongoose.Types.ObjectId(),
-    title: 'test',
-    description: 'test',
-    priority: 10,
-    status: TASK_STATUS.IN_PROGRESS,
-    createdAt: new Date(),
-    updatedAt: new Date()
-  })
 
   beforeAll(async () => {
-    taskMongooseRepository = {
-      create: jest.fn().mockResolvedValue(createResultMock)
-    }
-
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [MainModule]
-    })
-      .overrideProvider(TaskMongooseRepository)
-      .useValue(taskMongooseRepository)
-      .compile()
+    }).compile()
 
     app = moduleFixture.createNestApplication()
+    Logger.overrideLogger([])
     await app.init()
   })
 
@@ -51,10 +31,10 @@ describe('TaskCreateController (e2e)', () => {
     expect(response.status).toEqual(201)
     expect(response.body).toEqual({
       id: expect.any(String),
-      title: 'test',
-      description: 'test',
-      priority: 10,
-      status: 'in_progress',
+      title: input.title,
+      description: input.description,
+      priority: input.priority,
+      status: 'pending',
       createdAt: expect.any(String),
       updatedAt: expect.any(String)
     })
@@ -74,13 +54,13 @@ describe('TaskCreateController (e2e)', () => {
     })
   })
 
-  it('/task (POST 500) should throw an error with internal server errr', async () => {
+  it('/task (POST 500) should throw an error with internal server error', async () => {
     const input = {
       title: 'Create project board 2',
       description: 'Create the board in trello for the project X',
       priority: 5
     }
-    taskMongooseRepository.create?.mockRejectedValueOnce(new Error('test'))
+    jest.spyOn(MongooseRepositoryAbstract.prototype, 'create').mockRejectedValueOnce(new Error('test'))
     const response = await request(app.getHttpServer() as App)
       .post('/task')
       .send(input)
@@ -93,5 +73,9 @@ describe('TaskCreateController (e2e)', () => {
 
   afterAll(async () => {
     await app.close()
+  })
+
+  afterEach(() => {
+    jest.restoreAllMocks()
   })
 })
